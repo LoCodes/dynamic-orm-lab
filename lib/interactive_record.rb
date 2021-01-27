b217c9c0377e7283894d09209268102ff87a3ef0
Loren Cipriano <loren25cipriano@gmail.com>
2021-01-27 14:08:25 -0800

    commit tree b7975e11191a39acdcce77ef6302c1232b21ebe0

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,84 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+    # .table_name
+    # creates a downcased, plural table name based on the Class name 
+
+    def self.table_name
+        "#{self.to_s.downcase}s"
+    end
+
+
+    # .column_names
+    # returns an array of SQL column names
+    def self.column_names    
+        sql = "pragma table_info('#{table_name}')"
+    
+        table_info = DB[:conn].execute(sql)
+        column_names = []
+        table_info.each do |row|
+          column_names << row["name"]
+        end
+        column_names.compact
+    end 
+
+    def save
+        #sql = ()
+        DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    end
+
+    
+
+
+    # initialize
+    # creates an new instance of a student
+    # creates a new student with attributes 
+
+    def initialize(options={})
+        options.each do |property, value|
+          self.send("#{property}=", value)
+        end
+    end 
+    
+    # #table_name_for_insert
+    # return the table name when called on an instance of Student
+
+    def table_name_for_insert
+        self.class.table_name
+    end 
+
+#    #values_for_insert
+#    formats the column names to be used in a SQL statement 
+
+    def values_for_insert
+        values = []
+        self.class.column_names.each do |col_name|
+            values << "'#{send(col_name)}'" unless send(col_name).nil?
+        end 
+        values.join(", ")
+    end 
+
+    # #col_names_for_insert
+    # return the column names when called on an instance of Student
+    # does not include an id column
   
-end
+
+    def col_names_for_insert
+        self.class.column_names.delete_if {|col| col == "id"}.join(", ") 
+    end 
+
+    def self.find_by_name(name)
+        sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+        DB[:conn].execute(sql)
+    end
+
+    def self.find_by(attribute_hash)
+        value = attribute_hash.values.first
+        formatted_value = value.class == Fixnum ? value : "'#{value}'"
+        sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+        DB[:conn].execute(sql) 
+    end 
+
+end 
--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,13 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
-class Student < InteractiveRecord
 
+# initialize
+# creates a new student with attributes
+
+
+class Student < InteractiveRecord
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+    end
 end
